4b6bdd541a514197be56ddb9ac74418a25e8dfda
shohan12727 <aishohan001@gmail.com>
2025-12-26 20:36:36 +0600

    added

---
+++ next-3/src/app/about/contact/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/Components/Title';
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <Title>My Contact is 0198493489389</Title>
+        </div>
+    );
+};
+
+export default About;
