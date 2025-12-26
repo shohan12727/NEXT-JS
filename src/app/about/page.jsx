3f3c4f41fd3f62f7d32ed4f408c9ba460f8bd4ff
shohan12727 <aishohan001@gmail.com>
2025-12-26 10:18:54 +0600

    created different kinds of routing

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/Components/Title';
+import React from 'react';
+
+const about = () => {
+    return (
+        <div>
+            <Title>Welcome to about</Title>
+        </div>
+    );
+};
+
+export default about;
