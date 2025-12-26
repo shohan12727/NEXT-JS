93ce27881b09737ee38ef7bc8025505030bc8fb6
shohan12727 <aishohan001@gmail.com>
2025-12-26 23:44:15 +0600

    leading added

---
+++ next-3/src/app/loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div>
+      <div className="min-h-screen flex justify-center items-center text-8xl">
+        <h2>L</h2> <span className="animate-spin">🎯</span>
+        <h2>ADING</h2>
+      </div>
+    </div>
+  );
+};
+
+export default loading;
