0c53875f750a7d4cd180b9d4392ec943cae30065
shohan12727 <aishohan001@gmail.com>
2025-12-27 12:57:07 +0600

    updated

---
+++ next-4/src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import logo from "@/assets/my_photo.jpg";
+
+export default function Home() {
+  return (
+    <>
+      <div className="min-h-screen w-full flex justify-center items-center bg-sky-200">
+        <h2 className="text-4xl">Next js fetching and rendering</h2>
+      </div>
+    </>
+  );
+}
